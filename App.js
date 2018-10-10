96f13795b5397aa32316332882593037f3a800c7
Gonza Clavell <gonzaloclavell@gmail.com>
2018-10-10 15:18:55 -0300

    redirectUri va a la app y se loguea el code en la consola

--- App.js
+++ App.js
@@ -28,18 +28,35 @@ import { Constants } from 'expo';
 //oauth2.marathon.l4lb.thisdcos.directory:9999
 //http://oauth2.marathon.l4lb.thisdcos.directory:9999/oauth/private/v1/authorize?response_type=code&client_id=form&scope=read&redirect_uri={{urlOAuth}}
 
-  const source = {
-    uri: 'http://198.18.128.169:39044/oauth/private/v1/authorize?response_type=code&client_id=form&scope=read&redirect_uri=localhost:9999',
-    headers: {
-      "Authorization": "Basic Zm9ybTpmb3Jtc2VjcmV0" // base64 of 'foo:bar'
-    }
+  const env = {
+    urlBase: 'http://198.18.128.169:39009/oauth/private/v1',
+    redirectUri: 'exp://198.18.128.169:19000'
+    
   };
 
 export default class App extends Component {
 
   constructor (props, context) {
     super(props);
-    this.state = {texto: "Hola"};
+    this.state = {
+      code: "",
+      getTokenUrl: env.urlBase+'/token?grant_type=authorization_code&client_id=form&client_secret=formsecret&retredirect_uri='+env.redirectUri,
+      authorizeUrl: env.urlBase+'/authorize?response_type=code&client_id=form&scope=read&redirect_uri='+env.redirectUri
+    };
+  }
+
+  componentDidMount() {
+    console.log("agrego listener");
+    Linking.addEventListener('url', this.handleOpenURL);
+  }
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this.handleOpenURL);
+  }
+  handleOpenURL(event) {
+    console.log(event.url);
+    const route = e.url.replace(/.*?:\/\//g, '');
+    console.log("ROUTE "+route);
+    // do something with the url, in our case navigate(route)
   }
 
   render() {
@@ -51,10 +68,29 @@ export default class App extends Component {
         justifyContent: 'center'
       }}>
       <View>
+        <Text>
+          authorizeUrl :{this.state.authorizeUrl}
+        </Text>
         <Button
-          onPress={ () => { Linking.openURL('http://198.18.128.169:39009/oauth/private/v1/authorize?response_type=code&client_id=form&scope=read&redirect_uri=http://www.google.com') }}
+          onPress={ () => { 
+              Linking.openURL(this.state.authorizeUrl) 
+            }
+          }
           title="TP CONNECT"
         />
+  
+
+        <Text>
+          getTokenUrl :{this.state.getTokenUrl}
+        </Text>
+        <Button
+          onPress={ () => { 
+              Linking.openURL(this.state.getTokenUrl+this.state.code) 
+            }
+          }
+          title="GET TOKEN"
+        />
+        <Text></Text>
       </View>
 
       </View> 
